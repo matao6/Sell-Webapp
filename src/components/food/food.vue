176323031dd61197b85a5d5e67311d679d725389
matao6 <1152843670@qq.com>
2017-06-12 20:19:13 +0800

    add ratingselect components in food.vue

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -18,13 +18,23 @@
                         <span class="now">￥{{ food.price }}</span>
 						<span class="old" v-show="food.oldPrice">￥{{ food.oldPrice }}</span>
                     </div>
+                    <div class="cartcontrol-wrapper">
+                        <cartcontrol :food="food"></cartcontrol>
+                    </div>
+                    <transition name="fade">
+                        <div class="buy" @click="addFirst" v-show="!food.count || food.count === 0">加入购物车</div>
+                    </transition>
                 </div>
-                <div class="cartcontrol-wrapper">
-                    <cartcontrol :food="food"></cartcontrol>
+                <split></split>
+                <div class="info" v-show="food.info">
+                    <h1 class="title">商品介绍</h1>
+                    <p class="text">{{ food.info }}</p>
                 </div>
-                <transition name="fade">
-                    <div class="buy" @click="addFirst" v-show="!food.count || food.count === 0">加入购物车</div>
-                </transition>
+                <split v-show="food.info"></split>
+                <div class="rating">
+                    <h1 class="title">商品评价</h1>
+                    <ratingselect :select-type="selectType" :only-content="onlyContent" :desc="desc" :ratings="food.ratings"></ratingselect>
+                </div>
             </div>
         </div>
     </transition>
@@ -35,6 +45,11 @@
     import BScroll from 'better-scroll'
     import cartcontrol from 'components/cartcontrol/cartcontrol'
     import split from 'components/split/split'
+    import ratingselect from 'components/ratingselect/ratingselect'
+
+    // const POSITIVE = 0
+    // const NEGATIVE = 1
+    const ALL = 2
 
     export default {
         props: {
@@ -44,11 +59,21 @@
         },
         data () {
             return {
-                showFlag: false
+                showFlag: false,
+                selectType: ALL,
+                onlyContent: true,
+                desc: {
+                    all: '全部',
+                    positive: '推荐',
+                    negative: '吐槽'
+                }
             }
         },
         methods: {
             show () {
+                this.selectType = ALL
+                this.onlyContent = true
+
                 this.showFlag = true
                 this.$nextTick(() => {
                     if (!this.scroll) {
@@ -73,7 +98,8 @@
         },
         components: {
             cartcontrol,
-            split
+            split,
+            ratingselect
         }
     }
 </script>
@@ -112,6 +138,7 @@
                     font-size: 20px
                     color: #fff
         .content
+            position: relative
             padding: 18px
             font-size: 0
             .title
@@ -143,25 +170,37 @@
                     color: rgb(147, 153, 159)
                     line-height: 24px
                     text-decoration: line-through
-        .cartcontrol-wrapper
-            position: absolute
-            right: 12px
-            bottom: 12px
-        .buy
-            position: absolute
-            right: 18px
-            bottom: 18px
-            z-index: 10
-            height: 24px
-            line-height: 24px
-            padding: 0 12px
-            box-sizing: border-box
-            border-radius: 12px
-            font-size: 10px
-            color: #fff
-            background: rgb(0, 160, 220)
-            &.fade-enter-active, &.fade-leave-active
-                transition: all 0.5s
-            &.fade-enter, &.fade-leave-active
-                opacity: 0
+            .cartcontrol-wrapper
+                position: absolute
+                right: 12px
+                bottom: 12px
+            .buy
+                position: absolute
+                right: 18px
+                bottom: 18px
+                z-index: 10
+                height: 24px
+                line-height: 24px
+                padding: 0 12px
+                box-sizing: border-box
+                border-radius: 12px
+                font-size: 10px
+                color: #fff
+                background: rgb(0, 160, 220)
+                &.fade-enter-active, &.fade-leave-active
+                    transition: all 0.5s
+                &.fade-enter, &.fade-leave-active
+                    opacity: 0
+        .info
+            padding: 18px
+            .title
+                font-size: 14px
+                line-height: 14px
+                color: rgb(7, 17, 27)
+            .text
+                padding: 6px 8px 0 8px
+                font-size: 12px
+                font-weight: 200px
+                color: rgb(77, 85, 93)
+                line-height: 24px
 </style>
