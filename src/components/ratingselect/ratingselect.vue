079ef9ae19404c3fe781e929a02b0f9a49438161
matao6 <1152843670@qq.com>
2017-07-10 10:10:40 +0800

    end ratindselect component

--- src/components/ratingselect/ratingselect.vue
+++ src/components/ratingselect/ratingselect.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="ratingselect">
-        <div class="rating-type">
-            <span>{{ desc.all }}</span>
-            <span>{{ desc.positive }}</span>
-            <span>{{ desc.negative }}</span>
+        <div class="rating-type border-1px">
+            <span @click="select(2, $event)" class="block positive" :class="{'active': selectType === 2}">{{ desc.all }}<span class="count">{{ratings.length}}</span></span>
+            <span @click="select(0, $event)" class="block positive" :class="{'active': selectType === 0}">{{ desc.positive }}<span class="count">{{positives.length}}</span></span>
+            <span @click="select(1, $event)" class="block negative" :class="{'active': selectType === 1}">{{ desc.negative }}<span class="count">{{negatives.length}}</span></span>
         </div>
-        <div class="switch">
+        <div @click="toggleContent" class="switch" :class="{'on': onlyContent}">
             <span class="icon-check_circle"></span>
             <span class="text">只看用内容的评价</span>
         </div>
@@ -13,8 +13,8 @@
 </template>
 
 <script>
-// const POSITIVE = 0
-// const NEGATIVE = 1
+const POSITIVE = 0
+const NEGATIVE = 1
 const ALL = 2
 
 export default {
@@ -43,11 +43,84 @@ export default {
                 }
             }
         }
+    },
+    computed: {
+        positives () {
+            return this.ratings.filter((rating) => {
+                return rating.rateType === POSITIVE
+            })
+        },
+        negatives () {
+            return this.ratings.filter((rating) => {
+                return rating.rateType === NEGATIVE
+            })
+        }
+    },
+    methods: {
+        select (type, event) {
+            if (!event._constructed) {
+                return
+            }
+            let selectType = type
+            this.$root.eventHub.$emit('ratingtype.select', selectType)
+        },
+        toggleContent (event) {
+            if (!event._constructed) {
+                return
+            }
+            let toggle = !this.onlyContent
+            this.$root.eventHub.$emit('content.toggle', toggle)
+        }
     }
 }
 </script>
 
 <style lang="stylus">
+    @import "../../common/stylus/mixin.styl"
+
     .ratingselect
-        .rating
+        .rating-type
+            padding: 18px 0
+            margin: 0 18px
+            border-1px(rgba(7, 17, 27, 0.1))
+            font-size: 0
+            .block
+                display: inline-block
+                padding: 8px 12px
+                font-size: 12px
+                line-height: 16px
+                margin-right: 8px
+                border-radius: 2px
+                color: rgb(77, 85, 93)
+                &.active
+                    color: #fff
+                &.positive
+                    background: rgba(0, 160, 220, 0.2)
+                    &.active
+                        background: rgb(0, 160, 220)
+                &.negative
+                    background: rgba(77, 85, 93, 0.2)
+                    &.active
+                        background: rgb(77, 85, 93)
+                .count
+                    margin-left: 2px
+                    font-size: 8px
+        .switch
+            padding: 12px 18px
+            line-height: 24px
+            color: rgb(147, 153, 159)
+            border-bottom: 1px solid rgba(7, 17, 27, 0.1)
+            font-size: 0
+            &.on
+                .icon-check_circle
+                    color: #00c850
+            .icon-check_circle
+                display: inline-block
+                vertical-align: top
+                font-size: 24px
+                margin-right: 4px
+            .text
+                display: inline-block
+                // vertical-align: top
+                font-size: 12px
 </style>
